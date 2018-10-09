866687b8ab86b1bcb205611debfe1168e26c1e8e
Oleg Zorin <o_zorin@dpdok.com.ua>
2018-10-09 16:57:46 +0300

    add logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from '../reducers'
+import logger from '../middlewares/logger'
 
 const configureStore = () => {
   return createStore(
     rootReducer,
-    applyMiddleware( thunkMiddleware )
+    applyMiddleware(
+      thunkMiddleware, 
+      logger )
   )
 }
 
