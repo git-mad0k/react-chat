d3802d63af1044750f4ed067615f47157dfbd227
Oleg Zorin <o_zorin@dpdok.com.ua>
2018-10-16 12:09:25 +0300

    removed withStyles

--- src/components/ChatItem.js
+++ src/components/ChatItem.js
@@ -2,15 +2,13 @@ import React from 'react'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import AvatarProfile from './AvatarProfile'
-import { withStyles } from '@material-ui/core/styles'
 
 
-
-const ChatItem = ({ classes, name, color, date, key }) => (
+const ChatItem = ({ name, color, date, key }) => (
   <ListItem key={key} button>
     <AvatarProfile name={name} color={color} />
     <ListItemText primary={name} secondary={date} />
   </ListItem>
 )
 
-export default withStyles()(ChatItem)
+export default ChatItem
