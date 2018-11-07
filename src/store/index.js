8a35dfeb5b621d6c1f6bdbb352668e810b8a88f9
Oleg Zorin <o_zorin@dpdok.com.ua>
2018-11-07 11:34:52 +0200

    fix store

--- src/store/index.js
+++ src/store/index.js
@@ -3,24 +3,22 @@ import thunkMiddleware from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from '../reducers';
 
-
 const configureStore = () => {
   if (process.env.NODE_ENV === 'production') {
     const enhancer = applyMiddleware(thunkMiddleware);
-    return createStore(enhancer);
+    return createStore(rootReducer, enhancer);
   }
-  const composeEnhancers = typeof window === 'object'
-  /* eslint-disable no-underscore-dangle */
-        && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      serialize: true,
-    }) : compose;
+  const composeEnhancers =
+    typeof window === 'object' &&
+    /* eslint-disable no-underscore-dangle */
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        serialize: true,
+      })
+      : compose;
   /* eslint-enable no-underscore-dangle */
   const enhancer = composeEnhancers(applyMiddleware(thunkMiddleware, logger));
-  const store = createStore(
-    rootReducer,
-    enhancer,
-  );
+  const store = createStore(rootReducer, enhancer);
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {
